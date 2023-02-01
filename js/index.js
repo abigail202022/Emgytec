2c97ce705c88aac1e403edc1b99bc39e29b6b0c5
abigail.aa.98@hotmail.com <abigail.aa.98@hotmail.com>
2023-01-31 22:36:01 -0500

    no sé

---
+++ js/index.js
@@ -0,0 +1,91 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '-100px'
+    });   
+
+    sr.reveal('.slide', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '-100px'
+    }); 
+
+    sr.reveal('.portada', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '-100px'
+    }); 
+
+    sr.reveal('.portadaC', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '-100px'
+    }); 
+
+    sr.reveal('.tituPort', {
+        duration: 4000,
+        origin: 'left',
+        distance: '400px'
+    }); 
+
+    sr.reveal('.clima', {
+        duration: 4000,
+        origin: 'left',
+        distance: '400px'
+    });
+
+    sr.reveal('.wrap', {
+        duration: 4000,
+        origin: 'left',
+        distance: '400px'
+    }); 
+
+    sr.reveal('.scroll-auto', {
+        duration: 4000,
+        origin: 'top',
+        distance: '400px'
+    }); 
+
+    sr.reveal('.formulario', {
+        duration: 4000,
+        origin: 'right',
+        distance: '400px'
+    }); 
+
+    sr.reveal('.scroll-energia', {
+        duration: 4000,
+        origin: 'top',
+        distance: '400px'
+    }); 
+
+    sr.reveal('.scroll-mante', {
+        duration: 4000,
+        origin: 'top',
+        distance: '400px'
+    });
+
+    sr.reveal('.scroll-pro', {
+        duration: 4000,
+        origin: 'top',
+        distance: '400px'
+    });
+
+    sr.reveal('.nosotros', {
+        duration: 4000,
+        origin: 'left',
+        distance: '400px'
+    }); 
+
+    sr.reveal('.contenidos', {
+        duration: 4000,
+        origin: 'right',
+        distance: '400px'
+    }); 
+
+    sr.reveal('.pie', {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '500px'
+    }); 
